1a19155e0d354490a0c6a6e4ff1a34e8a69d1f0e
AyoubESI <ahmedayoub2001@gmail.com>
2021-04-22 21:40:37 +0100

    added sendLocation method, it should be called whenever the system detects the reception of the secret message

--- app/src/main/java/com/example/carapp/MainActivity.java
+++ app/src/main/java/com/example/carapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.carapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendLocation () {
+        //both SEND_SMS and ACCESS_FINE_LOCATION permissions should be checked before calling this function
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //initialize location
+                Location location = task.getResult();
+                if (location!=null){
+                    try {
+                        //initialize geocoder
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                        //initialize address list
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                        //sending location via sms
+                        String message = "Latitude :" + addresses.get(0).getLatitude() + "\nLongitude : "+ addresses.get(0).getLongitude();
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
